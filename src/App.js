333e32323615a1e4277af9572ad3888262d27073
amh6281 <amh6281@naver.com>
2021-12-27 16:17:53 +0900

    Feat: GlobalStyle.js 추가 2021.12.27
    createGlobalStyle을 사용하여 전체적으로 필요한 태그를 스타일링한다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+//Styles
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
