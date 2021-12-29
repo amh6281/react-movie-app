75a5644dbe280406d8b8b2128ff7888155d982fa
amh6281 <amh6281@naver.com>
2021-12-29 13:15:24 +0900

    Feat: 영화 id 가져오기 2021.12.29
    영화 목록에서 영화를 클릭했을 경우, 해당되는 영화의 id를 가져오기 위해 useMovieFetch.js 생성

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ const App = () => (
       <Route path="/:movieId" element={<Movie />} />
       <Route path="/*" element={<NotFound />} />
     </Routes>
-    <Home />
     <GlobalStyle />
   </Router>
 );
